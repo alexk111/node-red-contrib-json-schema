d2d46aef47b266c63a0b1867218f743f759df034
alexk111 <gh@alexkaul.com>
2024-12-14 18:02:30 +0400

    Upd

--- README.md
+++ README.md
@@ -1,37 +1,12 @@
 **Built upon [node-red-contrib-full-msg-json-schema-validation](https://github.com/oarroyog/node-red-contrib-json-schema), origial readme follows**
 
-This validation node does two more things:
+This validation node does one more thing:
 
 - valid the *entire* object, previously it was only possible to validate a property on the object
-- generates documentation of the schema and replaces the info details of the node with the documentation
 
 Some more details are available at the comparison [flow](https://flowhub.org/f/36690f145d5af6ca) and the forum [discussion](https://discourse.nodered.org/t/requiring-a-msg-structure-enforcing-the-presence-of-fields-and-attributes/81432).
 
 Unlike the original, instead of two outputs this has only one. Previously the two outputs where used to diverge messages that where valid and those that weren't. Instead this throws an exception that can be caught if validation fails. For the author this makes more sense since a validation that fails represents an unknown state of the system, likely to cause failure - fail fast, fail early is the motto.
-
-Documentation is created using [jsonschema2md](https://github.com/adobe/jsonschema2md) and stored in the nodes *info* box - **existing content will be replaced**. The intention is to copy and paste that documentation to somewhere else. The info box is a good place to put it in the first place, alternative would be a debug message of some sort.
-
-To generate the documentation, use the button in the property panel:
-
-![img](https://cdn.openmindmap.org/content/1695920371650_Screen_Shot_2023-09-28_at_18.58.34.png)
-
-The schema can be pasted into the editor pane also located in the property panel:
-
-![img](https://cdn.openmindmap.org/content/1695920460280_Screen_Shot_2023-09-28_at_19.00.38.png)
-
-## Documentation
-
-Documentation can be found in the description panel and from there copied:
-
-![img](https://cdn.openmindmap.org/content/1695985694935_Screen_Shot_2023-09-29_at_13.07.16.png)
-
-The markdown is rendered in the info box of the node:
-
-![img](https://cdn.openmindmap.org/content/1695985680328_Screen_Shot_2023-09-29_at_13.07.25.png)
-
-## Examples
-
-An example flow is included and can also be viewed [here](https://flowhub.org/f/f21aed28a04a7fd0).
 
 ## Validation methods
 
@@ -78,8 +53,8 @@ Just open node properties and choose which property object wants to validate and
 }
 
 Examples:
-- OK 
-msg.payload= 
+- OK
+msg.payload=
 {
   "firstName": "John",
   "lastName": "Doe",
@@ -87,7 +62,7 @@ msg.payload=
 };
 
 - KO
-msg.payload= 
+msg.payload=
 {
   "firstName": "John",
   "age": 1
--- schema.js
+++ schema.js
@@ -2,7 +2,6 @@ module.exports = function (RED) {
     "use strict";
     function JsonSchemaValidator(n) {
         RED.nodes.createNode(this, n);
-        this.func = n.func;
         this.name = n.name;
         this.property = n.property;
         this.propertyType = n.propertyType;
@@ -16,7 +15,7 @@ module.exports = function (RED) {
 
         node.on('input', function (msg, send, done) {
             try {
-                var schema = typeof node.func === 'string' && node.func.trim().length ? JSON.parse(node.func) : typeof msg.schema === 'string' ? JSON.parse(msg.schema) : msg.schema;                
+                var schema = typeof msg.schema === 'string' ? JSON.parse(msg.schema) : msg.schema;
 
                 var ajv = new Ajv({
                     allErrors: true,
@@ -54,24 +53,24 @@ module.exports = function (RED) {
                     }
 
                     switch (node.propertyType) {
-                        case "env":    
-                        case "fullenv":    
-                            obj = process.env;  
-                            break;
-                            
-                        case "msg":    
-                        case "fullmsg":    
-                            obj = msg;          
+                        case "env":
+                        case "fullenv":
+                            obj = process.env;
                             break;
 
-                        case 'flow':   
-                        case 'fullflow':   
-                            obj = flowOrGlobalToHash(node.context().flow);   
+                        case "msg":
+                        case "fullmsg":
+                            obj = msg;
                             break;
 
-                        case 'global': 
-                        case 'fullglobal': 
-                            obj = flowOrGlobalToHash(node.context().global); 
+                        case 'flow':
+                        case 'fullflow':
+                            obj = flowOrGlobalToHash(node.context().flow);
+                            break;
+
+                        case 'global':
+                        case 'fullglobal':
+                            obj = flowOrGlobalToHash(node.context().global);
                             break;
 
                         default:
@@ -97,38 +96,4 @@ module.exports = function (RED) {
     }
     RED.nodes.registerType("JsonSchemaValidatorWithDocu", JsonSchemaValidator);
 
-    RED.httpAdmin.post("/JsonSchemaValidatorWithDocu/:id",
-        RED.auth.needsPermission("JsonSchemaValidatorWithDocu.write"),
-        (req, res) => {
-            var node = RED.nodes.getNode(req.params.id);
-            if (node != null) {
-                try {
-                    if (req.body && node.type == "JsonSchemaValidatorWithDocu") {
-                        import('@adobe/jsonschema2md').then( (module) => {
-                            const markdown = module.jsonschema2md(req.body, {
-                                includeReadme: false,
-                            });
-
-                            res.status(200).send({
-                                md: markdown
-                            })
-                        }).catch(err => {
-                            console.error(err);
-                            res.status(500).send(err.toString());
-                            node.error("SchemaDocu: Submission failed: " +
-                                err.toString())
-                        })                    
-                    } else {
-                        res.sendStatus(404);
-                    }
-                } catch (err) {
-                    console.error(err);
-                    res.status(500).send(err.toString());
-                    node.error("JsonSchemaValid: Submission failed: " +
-                        err.toString())
-                }
-            } else {
-                res.sendStatus(404);
-            }
-        });
 };
